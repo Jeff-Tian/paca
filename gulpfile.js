8c438ad3c90d80865386df29181f024cecefc1c2
田杰 <jefftian@hcdlearning.com>
2016-07-11 18:27:00 +0800

    add 360 resign task to gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -95,16 +95,28 @@ gulp.task('version', function (done) {
 
 gulp.task('android-release', function (done) {
     var fileName = 'paca_' + getAppVersion() + '.apk';
-    var apkPath = '';
+    var apkPath = '/Users/tianjie/paca/platforms/android/build/outputs/apk/android-release-unsigned.apk';
 
     sh.rm(fileName);
 
     sh.exec('ionic build --release android', function () {
-        done();
-        return;
-
-        sh.exec('jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore -storepass 1050709 ' + apkPath + ' alias_name', function () {
+        sh.exec('jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore -storepass 1050709 ' + apkPath + ' paca', function () {
+            console.log('========= jarsigner done with ======');
+            console.log(arguments);
+            console.log('====================================');
             sh.exec('zipalign -v 4 ' + apkPath + ' ' + fileName, done);
         });
+    });
+});
+
+gulp.task('android-resign', function (done) {
+    var apkPath = './paca_0.0.3.encrypted.apk';
+    var fileName = 'paca_' + getAppVersion() + '_360encrypted.apk';
+
+    sh.exec('jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore -storepass 1050709 ' + apkPath + ' paca', function () {
+        console.log('========= jarsigner done with ======');
+        console.log(arguments);
+        console.log('====================================');
+        sh.exec('zipalign -v 4 ' + apkPath + ' ' + fileName, done);
     });
 });
